d8d0ac2512f010ea3bf7d98440a4e6083455da90
B.Mayr <b.mayr@we-make.ai>
2020-02-14 17:52:34 +0100

    deploying

--- app/server.py
+++ app/server.py
@@ -10,9 +10,7 @@ from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 from starlette.routing import Route
-import nest_asyncio
 
-nest_asyncio.apply()
 export_file_url = 'https://drive.google.com/uc?export=download&id=1U6vmC0eY_ejOvFvHIjXUsvI7Jsn31SRd'
 export_file_name = 'export.pkl'
 
